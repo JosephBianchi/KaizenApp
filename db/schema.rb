a37b1fbebba0b3e78609f5e58a839c39790b3c65
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-10-09 17:29:14 -0400

    create sub-category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008230645) do
+ActiveRecord::Schema.define(version: 20171009211807) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sub_categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_sub_categories_on_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
